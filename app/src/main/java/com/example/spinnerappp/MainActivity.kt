d092edbffe0c0b303e64d9d25298d32bb2689a8a
Gilbert Gotora <gilbertgotora2@gmail.com>
2022-01-10 05:08:52 +0200

    Add custom list with adapter and onItemSelectedListner

--- app/src/main/java/com/example/spinnerappp/MainActivity.kt
+++ app/src/main/java/com/example/spinnerappp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.example.spinnerappp.databinding.ActivityMainBinding
 
@@ -13,6 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val myList = listOf("One", "Two", "Three", "Four", "Five")
+        val adapter = ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, myList)
+        binding.spMyList.adapter = adapter
+
+        binding.spMyList.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(this@MainActivity, "You Selectes ${parent?.getItemAtPosition(position).toString()}", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
 
         binding.spMonths.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
