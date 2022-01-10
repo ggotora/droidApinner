483340732743ae0de23b9ad396931af338fce545
Gilbert Gotora <gilbertgotora2@gmail.com>
2022-01-10 04:54:37 +0200

    Add onItemSelectedListener

--- app/src/main/java/com/example/spinnerappp/MainActivity.kt
+++ app/src/main/java/com/example/spinnerappp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.spinnerappp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
 import com.example.spinnerappp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.spMonths.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(this@MainActivity,
+                    "You selected ${parent?.getItemAtPosition(position).toString()}", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
     }
 }
